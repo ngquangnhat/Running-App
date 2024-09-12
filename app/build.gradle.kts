0fdfebdb721d45bff1e598f326dfae971c82ca68
Nguyen Quang Nhat <nhatdzkk2211@gmail.com>
2024-09-12 22:29:25 +0700

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,106 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.safe.args)
+
+}
+
+android {
+    namespace = "com.example.runningapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.runningapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // Architectural Components
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Coroutine Lifecycle Scopes
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Navigation Components
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Glide
+    implementation(libs.glide)
+    ksp(libs.compiler)
+
+    // Google Maps Location Services
+    implementation(libs.play.services.location)
+    implementation(libs.play.services.maps)
+
+    // Dagger Core
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+
+    // Dagger Android
+    api(libs.dagger.android)
+    api(libs.dagger.android.support)
+    ksp(libs.dagger.android.processor)
+
+    // Easy Permissions
+    implementation(libs.easypermissions)
+
+    // Timber
+    implementation(libs.timber)
+
+    // MPAndroidChart
+    implementation(libs.mpandroidchart)
+
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/runningapp/db/Run.kt
@@ -0,0 +1,4 @@
+package com.example.runningapp.db
+
+class Run {
+}
